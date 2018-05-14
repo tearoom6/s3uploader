9fb5a7e050f382d8092367a9171349925f0180f1
Tomohiro Murota <tomohiro.murota@gmail.com>
2018-05-15 00:24:39 +0900

    Add drag & drop hook

--- spec/s3uploader-spec.js
+++ spec/s3uploader-spec.js
@@ -1,5 +1,7 @@
 'use babel';
 
+/* global atom, jasmine */
+
 import S3uploader from '../lib/s3uploader';
 
 // Use the command `window:run-package-specs` (cmd-alt-ctrl-p) to run specs.
@@ -15,59 +17,8 @@ describe('S3uploader', () => {
     activationPromise = atom.packages.activatePackage('s3uploader');
   });
 
-  describe('when the s3uploader:toggle event is triggered', () => {
-    it('hides and shows the modal panel', () => {
-      // Before the activation event the view is not on the DOM, and no panel
-      // has been created
-      expect(workspaceElement.querySelector('.s3uploader')).not.toExist();
-
-      // This is an activation event, triggering it will cause the package to be
-      // activated.
-      atom.commands.dispatch(workspaceElement, 's3uploader:toggle');
-
-      waitsForPromise(() => {
-        return activationPromise;
-      });
-
-      runs(() => {
-        expect(workspaceElement.querySelector('.s3uploader')).toExist();
-
-        let s3uploaderElement = workspaceElement.querySelector('.s3uploader');
-        expect(s3uploaderElement).toExist();
-
-        let s3uploaderPanel = atom.workspace.panelForItem(s3uploaderElement);
-        expect(s3uploaderPanel.isVisible()).toBe(true);
-        atom.commands.dispatch(workspaceElement, 's3uploader:toggle');
-        expect(s3uploaderPanel.isVisible()).toBe(false);
-      });
-    });
-
-    it('hides and shows the view', () => {
-      // This test shows you an integration test testing at the view level.
-
-      // Attaching the workspaceElement to the DOM is required to allow the
-      // `toBeVisible()` matchers to work. Anything testing visibility or focus
-      // requires that the workspaceElement is on the DOM. Tests that attach the
-      // workspaceElement to the DOM are generally slower than those off DOM.
-      jasmine.attachToDOM(workspaceElement);
-
-      expect(workspaceElement.querySelector('.s3uploader')).not.toExist();
-
-      // This is an activation event, triggering it causes the package to be
-      // activated.
-      atom.commands.dispatch(workspaceElement, 's3uploader:toggle');
-
-      waitsForPromise(() => {
-        return activationPromise;
-      });
-
-      runs(() => {
-        // Now we can test for view visibility
-        let s3uploaderElement = workspaceElement.querySelector('.s3uploader');
-        expect(s3uploaderElement).toBeVisible();
-        atom.commands.dispatch(workspaceElement, 's3uploader:toggle');
-        expect(s3uploaderElement).not.toBeVisible();
-      });
+  describe('when something happen', () => {
+    it('do something', () => {
     });
   });
 });
--- spec/s3uploader-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import S3uploaderView from '../lib/s3uploader-view';
-
-describe('S3uploaderView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
